4ccadc37751a4ac702803dfe00c9f8968c5286f5
SandeshBashyal <sandeshbashyal01@gmail.com>
2023-01-13 17:46:07 +0545

    CORS_ALLOW_ALL_ORIGINS is set true

--- frontend/src/Pages/NotesListPage.js
+++ frontend/src/Pages/NotesListPage.js
@@ -2,21 +2,27 @@ import React, {useState, useEffect} from 'react'
 //React hooks { }
 const NotesListPage = () => {
 
+    //3. Updated the state
     let [notes,setNotes] =useState([])  
+    //2. called in useEffect
     useEffect(() =>{
         getNotes()
     }, [])
 
+    //1. made a call
     let getNotes = async () =>{
         let response = await fetch('http://127.0.0.1:8000/api/notes/')
         let data = await response.json()
-        console.log('Data: ', data)
         setNotes(data)
     }
-
+        //4. rendered the data
     return (
         <div>
-            Notes
+            <div className='notes-list'>
+                {notes.map((note, index)=>{
+                    <h3 key ={index}>{note.body}</h3>
+                })}
+            </div>
         </div>
   )
 }
